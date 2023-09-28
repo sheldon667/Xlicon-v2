2cf8ee4571992a75b3333afa0d77fb48839de389
sheldon667 <144483791+sheldon667@users.noreply.github.com>
2023-09-28 14:01:48 +0000

    Update config.js 1

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
+global.Owner = ["221767262458"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["221767262458"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["221767262458"];
+global.OwnerName = "sheldon x";
 global.BotName = "XLICON-MD";
 global.packname = "X-2.0 Bot";                             //Do not change.
 global.author = "By: Slasher";                               //Do not change.
